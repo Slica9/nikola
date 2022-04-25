bace8bdf20f1e6b1622a12cc328dfea2971590a7
Nikola Basic <slica@Nikolas-MacBook-Pro.local>
2022-04-25 12:06:51 +0200

    Second commit.

---
+++ nikola.js
@@ -0,0 +1,15 @@
+const a = 12
+const x = '12'
+const b = 'nikola'
+
+function nikola() {
+    console.log('Hello Nikola')
+}
+
+function mirko() {
+    console.log('Hi Mirko')
+}
+
+const y = () => console.log(123)
+
+if (x == a) {}
